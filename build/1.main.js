0ca11fecae59bbc71dfb1e0f54cd77beb93c8665
mhartington <mikehartington@gmail.com>
2017-03-24 13:37:52 -0500

    fix(): fix help code Closes #47

--- build/1.main.js
+++ build/1.main.js
@@ -130,6 +130,7 @@ TimePipeModule = __decorate([
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_ionic_angular__ = __webpack_require__(50);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__providers_spotify_service_spotify_service__ = __webpack_require__(246);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_forms__ = __webpack_require__(13);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return SearchPage; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -143,6 +144,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
+
 var SearchPage = (function () {
     function SearchPage(nav, spotify, loadingCtrl) {
         this.nav = nav;
@@ -151,24 +153,15 @@ var SearchPage = (function () {
         this.listing = [];
         this.isError = false;
         this.showSpinner = false;
+        this.search = new __WEBPACK_IMPORTED_MODULE_3__angular_forms__["e" /* FormControl */]();
+        this.search.valueChanges
+            .subscribe(function (res) { return console.log(res); }, function () { return console.log('done'); });
     }
     SearchPage.prototype.detail = function (track) {
         this.nav.push('TrackDetailPage', {
             'id': track.id,
             'track': track
         });
-    };
-    SearchPage.prototype.doSearch = function (term) {
-        var _this = this;
-        this.showSpinner = true;
-        if (term) {
-            this.spotify.load(term)
-                .subscribe(function (results) { return _this.listing = results.tracks.items; }, function (error) { return _this.isError = true; }, function () { return _this.showSpinner = false; });
-        }
-        else {
-            this.listing = [];
-            this.showSpinner = false;
-        }
     };
     return SearchPage;
 }());
@@ -177,13 +170,12 @@ SearchPage = __decorate([
         segment: 'search'
     }),
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["J" /* Component */])({
-        selector: 'page-search',template:/*ion-inline-start:"/Users/mhartington/GitHub/StarTrack-Ionic/src/pages/search/search.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>Search</ion-title>\n  </ion-navbar>\n  <ion-toolbar>\n    <ion-searchbar\n      showCancelButton="true" (ionInput)="doSearch($event.target.value)">\n    </ion-searchbar>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content>\n  <div *ngIf="isError" text-center padding>\n    <ion-icon color="secondary" name="warning" big></ion-icon>\n    <h1>Uh-oh...</h1>\n    <h2>It\'s not me, it\'s you!</h2>\n    <h2>It appears there is a connection problem</h2>\n  </div>\n  <div padding text-center class="stauts-spinner" *ngIf="showSpinner">\n    <ion-spinner></ion-spinner>\n  </div>\n  <a ion-item *ngFor="let track of listing" detail-none (click)="detail(track)">\n    <ion-thumbnail item-left>\n      <img [src]="track.album.images[0].url" alt="">\n    </ion-thumbnail>\n    <h2>{{track.name}}</h2>\n    <h3>{{track.artists[0].name}}</h3>\n    <p>{{track.album.name}}</p>\n    <ion-note item-right>\n      {{track.duration_ms | msToMinutes}}\n    </ion-note>\n  </a>\n</ion-content>\n'/*ion-inline-end:"/Users/mhartington/GitHub/StarTrack-Ionic/src/pages/search/search.html"*/,
+        selector: 'page-search',template:/*ion-inline-start:"/Users/mhartington/GitHub/StarTrack-Ionic/src/pages/search/search.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>Search</ion-title>\n  </ion-navbar>\n  <ion-toolbar>\n    <ion-searchbar\n      [formControl]="search"\n      showCancelButton="true" >\n    </ion-searchbar>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content>\n  <div *ngIf="isError" text-center padding>\n    <ion-icon color="secondary" name="warning" big></ion-icon>\n    <h1>Uh-oh...</h1>\n    <h2>It\'s not me, it\'s you!</h2>\n    <h2>It appears there is a connection problem</h2>\n  </div>\n  <div padding text-center class="stauts-spinner" *ngIf="showSpinner">\n    <ion-spinner></ion-spinner>\n  </div>\n  <a ion-item *ngFor="let track of listing" detail-none (click)="detail(track)">\n    <ion-thumbnail item-left>\n      <img [src]="track.album.images[0].url" alt="">\n    </ion-thumbnail>\n    <h2>{{track.name}}</h2>\n    <h3>{{track.artists[0].name}}</h3>\n    <p>{{track.album.name}}</p>\n    <ion-note item-right>\n      {{track.duration_ms | msToMinutes}}\n    </ion-note>\n  </a>\n</ion-content>\n'/*ion-inline-end:"/Users/mhartington/GitHub/StarTrack-Ionic/src/pages/search/search.html"*/,
     }),
-    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_0_ionic_angular__["e" /* NavController */],
-        __WEBPACK_IMPORTED_MODULE_2__providers_spotify_service_spotify_service__["a" /* SpotifyService */],
-        __WEBPACK_IMPORTED_MODULE_0_ionic_angular__["i" /* LoadingController */]])
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_0_ionic_angular__["e" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_0_ionic_angular__["e" /* NavController */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_2__providers_spotify_service_spotify_service__["a" /* SpotifyService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__providers_spotify_service_spotify_service__["a" /* SpotifyService */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_0_ionic_angular__["i" /* LoadingController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_0_ionic_angular__["i" /* LoadingController */]) === "function" && _c || Object])
 ], SearchPage);
 
+var _a, _b, _c;
 //# sourceMappingURL=search.js.map
 
 /***/ })
