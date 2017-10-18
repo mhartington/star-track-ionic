a496cbce7da13fe4b2165e9191de73e03ff1844b
mhartington <mikehartington@gmail.com>
2017-10-18 14:53:37 -0400

    revert

--- src/pages/track-detail/track-detail.ts
+++ src/pages/track-detail/track-detail.ts
@@ -19,10 +19,7 @@ import { ItunesService } from '../../providers/itunes-service/itunes-service';
 })
 export class TrackDetailPage {
   @ViewChild('musicCard') musicCard;
-  track = {
-    artworkUrl100: 'data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==',
-    trackId: null
-  }
+  track;
   isFavorite = false;
   favoriteIcon = 'star-outline';
 
@@ -35,7 +32,7 @@ export class TrackDetailPage {
     public toastCtrl: ToastController,
     public service: ItunesService
   ) {}
-  ionViewDidEnter() {
+  ionViewWillEnter() {
     let paramsTrack = this.params.get('track');
     if (!paramsTrack) {
       this.loadSong();
