618ac02f356061cd4929f5ce8fd2f0f46071059e
mhartington <mikehartington@gmail.com>
2018-03-16 16:44:46 -0400

    yolo ;

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -2,13 +2,16 @@ module.exports = {
   modifyUrlPrefix: {
     '/': ''
   },
-  maximumFileSizeToCacheInBytes:5000000,
-  'globDirectory': 'www/',
-  'globPatterns': [
-    '**/*.{eot,scss,svg,ttf,woff,woff2,ico,png,js,css,html,json}'
-  ],
-  'swDest': './www/service-worker.js',
-  'globIgnores': [
-    '../workbox-cli-config.js'
+  maximumFileSizeToCacheInBytes: 5000000,
+  globDirectory: 'www/',
+  globPatterns: ['**/*.{eot,scss,svg,ttf,woff,woff2,ico,png,js,css,html,json}'],
+  swDest: './www/service-worker.js',
+  globIgnores: ['workbox-v3.0.0/**/*'],
+  // globIgnores: ['../workbox-cli-config.js', 'workbox-v3.0.0#<{(||)}>#*'],
+  runtimeCaching: [
+    {
+      urlPattern: '*',
+      handler: 'staleWhileRevalidate'
+    }
   ]
 };
