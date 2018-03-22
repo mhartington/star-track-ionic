76cb1611119dfe892eabfafb9c3b9d66441d774a
mhartington <mikehartington@gmail.com>
2018-03-22 16:11:02 -0500

    update

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   maximumFileSizeToCacheInBytes: 5000000,
   globDirectory: 'www/',
-  globPatterns: ['**/*.{eot,scss,svg,ttf,woff,woff2,ico,png,js,css,html,json}'],
+  globPatterns: ['**/*.{js,css,html,json}'],
   swDest: './www/service-worker.js',
   globIgnores: ['workbox-v3.0.0/**/*'],
   // globIgnores: ['../workbox-cli-config.js', 'workbox-v3.0.0#<{(||)}>#*'],
