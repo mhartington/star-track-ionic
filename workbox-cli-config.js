280457e92e45472ebfa0fdff8aa5d8b298bc6161
mhartington <mikehartington@gmail.com>
2018-03-22 16:08:55 -0500

    update

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -6,8 +6,8 @@ module.exports = {
   globDirectory: 'www/',
   globPatterns: ['**/*.{eot,scss,svg,ttf,woff,woff2,ico,png,js,css,html,json}'],
   swDest: './www/service-worker.js',
-  // globIgnores: ['workbox-v3.0.0#<{(||)}>#*'],
-  globIgnores: ['../workbox-cli-config.js', 'workbox-v3.0.0/**/*'],
+  globIgnores: ['workbox-v3.0.0/**/*'],
+  // globIgnores: ['../workbox-cli-config.js', 'workbox-v3.0.0#<{(||)}>#*'],
     runtimeCaching: [
     {
       urlPattern: '*',
